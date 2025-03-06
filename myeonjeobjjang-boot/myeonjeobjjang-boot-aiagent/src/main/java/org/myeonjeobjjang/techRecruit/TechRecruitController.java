00cd71ea6b9cf08a8be7e067119ade633d5112bf
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-03-06 17:35:31 +0900

    feat #3 : pdf 임베딩, rag기반 질의 API 엔드포인트 생성

--- myeonjeobjjang-boot/myeonjeobjjang-boot-aiagent/src/main/java/org/myeonjeobjjang/techRecruit/TechRecruitController.java
+++ myeonjeobjjang-boot/myeonjeobjjang-boot-aiagent/src/main/java/org/myeonjeobjjang/techRecruit/TechRecruitController.java
@@ -1,10 +1,11 @@
 package org.myeonjeobjjang.techRecruit;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.myeonjeobjjang.infra.ai.AiService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.myeonjeobjjang.infra.ai.dto.InfraAiRequest;
+import org.myeonjeobjjang.techRecruit.dto.TechRecruitRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,5 +15,22 @@ public class TechRecruitController {
     @GetMapping
     public String simpleQuestion(@RequestParam String question) {
         return aiService.requestSimpleQuestion(question);
+    }
+
+    @PostMapping("/pdf/embedding")
+    public Integer embeddingPdf(@RequestBody @Valid TechRecruitRequest.EmbeddingRequest request) {
+        return aiService.embeddingPdf(InfraAiRequest.EmbeddingPdfRequest.builder()
+            .fileName(request.fileName())
+            .userName(request.userName())
+            .build());
+    }
+
+    @PostMapping("/pdf/retrieve")
+    public Object retrievePdfResult(@RequestBody @Valid TechRecruitRequest.RetreieveRequest request) {
+        return aiService.retrievePdfResult(InfraAiRequest.TechRecruitmentRequest.builder()
+            .companyName(request.companyName())
+            .fileName(request.fileName())
+            .userName(request.userName())
+            .build());
     }
 }

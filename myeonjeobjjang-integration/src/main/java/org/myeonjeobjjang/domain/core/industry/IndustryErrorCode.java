b534b896cad4b7c67c1fa95e15c7bb89551d92b9
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-03-13 00:12:25 +0900

    feat #14 : 산업군 로직 예외처리

---
+++ myeonjeobjjang-integration/src/main/java/org/myeonjeobjjang/domain/core/industry/IndustryErrorCode.java
@@ -0,0 +1,17 @@
+package org.myeonjeobjjang.domain.core.industry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.myeonjeobjjang.exception.BaseErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum IndustryErrorCode implements BaseErrorCode {
+    INDUSTRY_NOT_FOUND("DOM_INDUSTRY_001","해당 산업군이 없습니다.", HttpStatus.UNAUTHORIZED),
+    DUPLICATED_INDUSTRY_NAME("DOM_INDUSTRY_002","이미 사용중인 산업군명입니다.", HttpStatus.IM_USED),
+    ;
+    private final String errorCode;
+    private final String message;
+    private final HttpStatus httpStatus;
+}
--- myeonjeobjjang-integration/src/main/java/org/myeonjeobjjang/domain/core/industry/service/IndustryServiceImpl.java
+++ myeonjeobjjang-integration/src/main/java/org/myeonjeobjjang/domain/core/industry/service/IndustryServiceImpl.java
@@ -5,7 +5,11 @@ import org.myeonjeobjjang.domain.core.industry.entity.Industry;
 import org.myeonjeobjjang.domain.core.industry.repository.IndustryRepository;
 import org.myeonjeobjjang.domain.core.industry.service.dto.IntegrationIndustryRequest;
 import org.myeonjeobjjang.domain.core.industry.service.dto.IntegrationIndustryResponse;
+import org.myeonjeobjjang.exception.BaseException;
 import org.springframework.stereotype.Service;
+
+import static org.myeonjeobjjang.domain.core.industry.IndustryErrorCode.DUPLICATED_INDUSTRY_NAME;
+import static org.myeonjeobjjang.domain.core.industry.IndustryErrorCode.INDUSTRY_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +17,8 @@ public class IndustryServiceImpl implements IndustryService {
     private final IndustryRepository industryRepository;
 
     public IntegrationIndustryResponse.IntegrationIndustryInfoResponse save(IntegrationIndustryRequest.IntegrationIndustryCreateRequest request) {
+        if(industryRepository.findIndustryByIndustryName(request.industryName()).isPresent())
+            throw new BaseException(DUPLICATED_INDUSTRY_NAME);
         Industry newIndustry = Industry.builder()
             .industryName(request.industryName())
             .industryInformation(request.industryInformation())
@@ -23,7 +29,7 @@ public class IndustryServiceImpl implements IndustryService {
 
     public IntegrationIndustryResponse.IntegrationIndustryInfoResponse get(Long industryId) {
         Industry industry = industryRepository.findById(industryId)
-            .orElseThrow();
+            .orElseThrow(() -> new BaseException(INDUSTRY_NOT_FOUND));
         return new IntegrationIndustryResponse.IntegrationIndustryInfoResponse(industry.getIndustryId(), industry.getIndustryName(), industry.getIndustryInformation());
     }
 }

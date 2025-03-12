f9b39c2693e32bba30c487dc29da81bd27a55d92
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-03-12 20:31:50 +0900

    feat #9 : 사용자 정보 요청 API 구현

--- myeonjeobjjang-boot/myeonjeobjjang-boot-aiagent/src/main/java/org/myeonjeobjjang/member/MemberController.java
+++ myeonjeobjjang-boot/myeonjeobjjang-boot-aiagent/src/main/java/org/myeonjeobjjang/member/MemberController.java
@@ -1,12 +1,15 @@
 package org.myeonjeobjjang.member;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.myeonjeobjjang.common.security.PrincipalDetails;
+import org.myeonjeobjjang.domain.core.member.entity.Member;
 import org.myeonjeobjjang.domain.core.member.service.MemberService;
 import org.myeonjeobjjang.domain.core.member.service.dto.IntegrationMemberRequest;
 import org.myeonjeobjjang.domain.core.member.service.dto.IntegrationMemberResponse;
 import org.myeonjeobjjang.member.dto.MemberRequest;
+import org.myeonjeobjjang.member.dto.MemberResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,5 +28,11 @@ public class MemberController {
     @PostMapping(value = "/sign-up", consumes = "application/json")
     public ResponseEntity<IntegrationMemberResponse.LoginOrSignUpResponse> signUp(@RequestBody @Validated MemberRequest.SignUpRequest request) {
         return ResponseEntity.ok(memberService.signUp(new IntegrationMemberRequest.IntegrationSignUpRequest(request.email(), request.userName())));
+    }
+
+    @GetMapping("/who")
+    public ResponseEntity<MemberResponse.MemberInfo> whoIsMe(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Member member = principalDetails.getMember();
+        return ResponseEntity.ok(new MemberResponse.MemberInfo(member.getMemberId(), member.getUserName(), member.getEmail(), member.getRole()));
     }
 }
